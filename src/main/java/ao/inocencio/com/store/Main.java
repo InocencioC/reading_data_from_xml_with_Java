8355e48b632a4b28e50d0306a4aa1dd11b217beb
Inocencio Cordeiro <cordeiroturorias@gmail.com>
2024-04-21 21:51:43 +0100

    add: now its possible return TOTAL GROSS SALE, TOTAL  DISCOUNT, TOTAL NET SALE.

--- src/main/java/ao/inocencio/com/store/Main.java
+++ src/main/java/ao/inocencio/com/store/Main.java
@@ -1,10 +1,11 @@
 package ao.inocencio.com.store;
 
-import javax.swing.*;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
+import java.math.BigDecimal;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,18 +16,20 @@ public class Main {
             Unmarshaller jaxbUnmarshaler = jaxbContext.createUnmarshaller();
             Vendas vendas = (Vendas) jaxbUnmarshaler.unmarshal(xmlFile);
 
-            for (Venda venda : vendas.getVenda()) {
-                System.out.println("ID da venda: "  + venda.getId());
-                System.out.println("Cliente: "  + venda.getClient());
-                System.out.println("Data da venda: "  + venda.getDate());
+            List<Venda> todasAsVendas = vendas.getVenda();
+            BigDecimal Grosstotal = BigDecimal.ZERO;
+            BigDecimal TotalDiscount = BigDecimal.ZERO;
 
-                for (Item item : venda.getItens()) {
-                    System.out.println("\tItem: "  + item.getProduct());
-                    System.out.println("\tQuantidade: "  + item.getQuantity());
-                    System.out.println("\tPreço: "  + item.getPrice());
-                    System.out.println("\tDesconto: "  + item.getDiscount());
+            for (Venda v : todasAsVendas) {
+                for (Item i : v.getItens()) {
+                 BigDecimal itemValue =  i.getPrice().multiply(new BigDecimal(i.getQuantity()));
+                    Grosstotal = Grosstotal.add(itemValue);
+                    TotalDiscount = TotalDiscount.add(i.getDiscount());
                 }
             }
+            System.out.println("TOTAL GROSS SALE " + Grosstotal);
+            System.out.println("TOTAL  DISCOUNT " + TotalDiscount);
+            System.out.println("TOTAL NET SALE " + Grosstotal.subtract(TotalDiscount));
         } catch (JAXBException e) {
             e.printStackTrace();
         }
